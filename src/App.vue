8fe1229fa45598ec8bd762b2c373b2f6f09157ca
AlvaroArbaiza <alvaro.xander@gmail.com>
2023-05-16 15:57:53 +0200

    Completamento progetto

--- src/App.vue
+++ src/App.vue
@@ -36,6 +36,13 @@ export default {
 
 <FooterComp/>
 
+<!-- back to top page -->
+<div class="backToTop">
+    <a href="#logo">
+        <i class="fa-solid fa-arrow-up"></i>
+    </a>
+</div>
+
 </template>
 
 <style lang="scss">
@@ -46,5 +53,25 @@ export default {
     width: 100%;
     background: url('/img/background-pattern-wavify.png');
     background-color: $amour;
+}
+
+.backToTop {
+    position: fixed;
+    right: 2rem;
+    bottom: 2rem;
+    width: 3rem;
+    aspect-ratio: 1;
+    background-color: $mountain-meadow;
+    border-radius: 50%;
+    text-align: center;
+    line-height: 3rem;
+
+    a {
+        display: block;
+        width: 100%;
+        aspect-ratio: 1;
+        text-decoration: none;
+        color: $white;
+    }
 }
 </style>
